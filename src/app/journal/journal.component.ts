379d5e0936d3821dd841bdf40d73e40f7b590d96
garlife <garlife@gmail.com>
2021-03-10 02:00:11 +0300

    lessons with borders

--- src/app/journal/journal.component.ts
+++ src/app/journal/journal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-journal',
@@ -7,7 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JournalComponent implements OnInit {
 
-  constructor() { }
+  chReadOnly=true;
+
+  jourForm: FormGroup;
+
+  constructor(private form: FormBuilder) {
+
+    this.jourForm = form.group({
+      marks: form.array([
+        form.group({
+          date: form.control('2021-01-01'),
+          theme: form.control('Тема урока 1'),
+          hw: form.control('Домашнее задание 1'),
+          notice: form.control('Без комментариев'),
+        }),
+      ]),
+    }); 
+  }
+
+  addLesson(){
+    (this.jourForm.get('lessons') as FormArray).push(this.form.group({
+      date: this.form.control(''),
+      theme: this.form.control(''),
+      hw: this.form.control(''),
+      notice: this.form.control(''),
+    }) )
+  }
+  delLesson(){
+    (this.jourForm.get('lessons') as FormArray).removeAt(length+1);
+  }
+
+  toggle(){
+    this.chReadOnly=!this.chReadOnly;
+  }
+
+  sendForm(){
+    console.log(this.jourForm)
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/journal/journal.module.ts
+++ src/app/journal/journal.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JournalComponent } from './journal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [JournalComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [JournalComponent]
 })
